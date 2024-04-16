8f7cf49303c77b1782327b7ff2534f3154af3882
howarc <108202241+howarc@users.noreply.github.com>
2024-04-15 22:37:33 -0400

    Dissected LinProg Matrix into Choices
    clueop.py currently does not complie;
    added some code that dissects choice of room, wep, person from linprog matrix

--- testclue.py
+++ testclue.py
@@ -1,6 +1,10 @@
 from scipy.optimize import linprog
 import numpy as np
 
+Rooms = ['Ball', 'Billiard', 'Conservatory', 'Dining', 'Hall', 'Kitchen', 'Lounge', 'Library', 'Study']
+Weapons = ['Knife', 'Pistol', 'Rope', 'Candlestick', 'Wrench', 'Lead_Pipe']
+People = ['White', 'Green', 'Scarlet', 'Mustard', 'Peacock', 'Plum']
+
 # define the coefficients for 3 suspects, 3 weapons, and 3 rooms
 # coefficients would be from 0-100, representing probability we know something ISN'T part of the murder
 # e.g. 100 means we def know it isn't part of the murder
@@ -20,9 +24,23 @@ A_ub = [
 b_ub = [-1, -1, -1]  # negative for the same reason as A_ub
 
 
-# run the linear programming algorithm to minimize the total cost
+# results.x will print out a 1d array of options
 result = linprog(c=coefficients, A_ub=A_ub, b_ub=b_ub, method='highs')
 
-# print the results
-print(result.x)
+
+# extract room option
+room_index = result.x[:len(Rooms)].argmax()
+room = Rooms[room_index]
+    
+# extract weapon option
+weapon_index = result.x[len(Rooms):len(Rooms) + len(Weapons)].argmax()
+weapon = Weapons[weapon_index]
+    
+# extract person option
+person_index = result.x[len(Rooms) + len(Weapons):].argmax()
+person = People[person_index]
+
+print(room, weapon, person) # remove later
+
+return room, weapon, person
 
